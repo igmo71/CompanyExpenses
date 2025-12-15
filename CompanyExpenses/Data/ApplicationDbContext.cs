68fea66f06a5d6dc925dc4ecd273ef39f1757e96
Igor Mogilnitskiy <igmo71@outlook.com>
2025-12-15 15:07:10 +0300

    76150 CompanyStructure

--- CompanyExpenses/Data/ApplicationDbContext.cs
+++ CompanyExpenses/Data/ApplicationDbContext.cs
@@ -7,11 +7,13 @@ namespace CompanyExpenses.Data
     public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options)
     {
         public DbSet<ExpenseItem> ExpenseItems { get; set; }
+        public DbSet<CompanyStructure> CompanyStructure { get; set; }
         protected override void OnModelCreating(ModelBuilder builder)
         {
             base.OnModelCreating(builder);
 
             builder.Entity<ExpenseItem>().HasKey(e => e.Id);
+            builder.Entity<CompanyStructure>().HasKey(e => e.Id);
         }
     }
 }
---
+++ CompanyExpenses/Domain/CompanyStructure.cs
@@ -0,0 +1,24 @@
+﻿using CompanyExpenses.Abstractions;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CompanyExpenses.Domain
+{
+    public class CompanyStructure : ITree<CompanyStructure>  
+    {
+        [MaxLength(36)]
+        public required string Id { get; set; }
+
+        [MaxLength(36)]
+        public string? ParentId { get; set; }
+
+        [MaxLength(150)]
+        public string? Name { get; set; }
+
+        [NotMapped]
+        public List<CompanyStructure> Children { get; set; } = [];
+
+        [NotMapped]
+        public bool Expanded { get; set; }
+    }
+}
